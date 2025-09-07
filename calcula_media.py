21fe934e9a4887038cf115f1639b6dddf13a968e
LeizaneMelo <60352187+LeizaneMelo@users.noreply.github.com>
2025-09-06 22:14:22 -0300

    Update calcula_media.py
    "Resolução exerc.04"

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,8 @@
-# Sua solução aqui
+numeros = []
+#Ler entradas
+for i in range(5):
+    num = float(input(f'Digite o {i+1}º número: ' ))
+    numeros.append(num)
+soma = sum(numeros)
+media = soma / len(numeros)
+print(f'{media:.2f}')
