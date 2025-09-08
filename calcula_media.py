7087943155510d9a53766aa4441c021f60ffadaf
LeizaneMelo <60352187+LeizaneMelo@users.noreply.github.com>
2025-09-07 22:31:07 -0300

    Update calcula_media.py
    "Adicionado comentários exer 04- calcula_media"

--- calcula_media.py
+++ calcula_media.py
@@ -1,8 +1,16 @@
+#Lista para armazenar os números
 numeros = []
+
 #Ler entradas
 for i in range(5):
+    
+    #Solicita numero e converte para float
     num = float(input(f'Digite o {i+1}º número: ' ))
-    numeros.append(num)
+    numeros.append(num) #Adiciona o número na lista
+    
+#Calacula a soma de todos números
 soma = sum(numeros)
-media = soma / len(numeros)
+media = soma / len(numeros) #Calcula média
+
+#Mostra resultado
 print(f'{media:.2f}')
